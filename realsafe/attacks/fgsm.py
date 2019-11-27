e436a3d3764d54456eaefe99d32606fd52aab36b
Fugoes <fugoes.qa@gmail.com>
2019-11-27 15:04:05 +0800

    refine FGSM

--- realsafe/attacks/fgsm.py
+++ realsafe/attacks/fgsm.py
@@ -6,29 +6,37 @@ from realsafe.attacks.utils import get_xs_ph, get_ys_ph, maybe_to_array, get_uni
 
 class FGSM(BatchAttack):
     """
-    TODO
+    Fast Gradient Sign Method (FGSM)
+    A white-box single-step constraint-based method. Require a differentiable loss function.
+
+    Supported distance metric: `l_2`, `l_inf`
+    Supported goal: `t`, `tm`, `ut`
+    Supported config parameters:
+    - `magnitude`: max distortion, could be either a float number or a numpy float number array with shape of
+        (batch_size,).
+
+    References:
+    [1] https://arxiv.org/abs/1412.6572
     """
 
     def __init__(self, model, batch_size, loss, goal, distance_metric, session):
         self.model, self.batch_size, self._session = model, batch_size, session
-
-        self.loss, self.goal = loss, goal
-        self.distance_metric = distance_metric
-
+        self.loss, self.goal, self.distance_metric = loss, goal, distance_metric
+        # placeholder for batch_attack's input
         self.xs_ph = get_xs_ph(model, batch_size)
         self.ys_ph = get_ys_ph(model, batch_size)
+        # magnitude
         self.eps_ph = tf.placeholder(self.model.x_dtype, (self.batch_size,))
         self.eps_var = tf.Variable(tf.zeros((self.batch_size,), dtype=self.model.x_dtype))
-
+        # calculate loss' gradient with relate to the input
+        grad = tf.gradients(self.loss(self.xs_ph, self.ys_ph), self.xs_ph)[0]
         if goal == 't' or goal == 'tm':
-            grad = -tf.gradients(self.loss(self.xs_ph, self.ys_ph), self.xs_ph)[0]
-        elif goal == 'ut':
-            grad = tf.gradients(self.loss(self.xs_ph, self.ys_ph), self.xs_ph)[0]
-        else:
+            grad = -grad
+        elif goal != 'ut':
             raise NotImplementedError
-
+        # flatten the gradient for easier broadcast operations
         grad_flatten = tf.reshape(grad, (batch_size, -1))
-
+        # calculate update
         if distance_metric == 'l_2':
             grad_unit = get_unit(grad_flatten)
             update = tf.expand_dims(self.eps_var, 1) * grad_unit
@@ -37,18 +45,19 @@ class FGSM(BatchAttack):
         else:
             raise NotImplementedError
         update = tf.reshape(update, (self.batch_size, *self.model.x_shape))
+        # clip by (x_min, x_max)
         self.xs_adv = tf.clip_by_value(self.xs_ph + update, self.model.x_min, self.model.x_max)
 
-        self.config_setup = self.eps_var.assign(self.eps_ph)
+        self.config_eps_step = self.eps_var.assign(self.eps_ph)
 
     def config(self, **kwargs):
-        eps = maybe_to_array(kwargs['magnitude'], self.batch_size)
-        self._session.run(self.config_setup, feed_dict={self.eps_ph: eps})
+        if 'magnitude' in kwargs:
+            eps = maybe_to_array(kwargs['magnitude'], self.batch_size)
+            self._session.run(self.config_eps_step, feed_dict={self.eps_ph: eps})
 
     def batch_attack(self, xs, ys=None, ys_target=None):
-        ls = ys if self.goal == 'ut' else ys_target
-
+        lbs = ys if self.goal == 'ut' else ys_target
         return self._session.run(self.xs_adv, feed_dict={
             self.xs_ph: xs,
-            self.ys_ph: ls
+            self.ys_ph: lbs,
         })
