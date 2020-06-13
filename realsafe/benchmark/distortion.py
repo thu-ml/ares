dc5aa792ca9265ad6e0ba2315c2a2661357c93c5
Fugoes <fugoes.qa@gmail.com>
2020-06-13 15:59:06 +0800

    add distortion benchmark for bim & mim

--- realsafe/benchmark/distortion.py
+++ realsafe/benchmark/distortion.py
@@ -52,7 +52,10 @@ class DistortionBenchmark(object):
 
         if self.attack_name == 'fgsm':
             self._logits = self.model.logits(self._xs_ph)
-            self._run = self._run_basic
+            self._run = self._run_binsearch
+        elif self.attack_name in ('bim', 'mim'):
+            self._logits = self.model.logits(self._xs_ph)
+            self._run = self._run_binsearch_alpha
         else:
             raise NotImplementedError
 
@@ -63,7 +66,7 @@ class DistortionBenchmark(object):
         '''
         self.attack.config(**kwargs)
 
-    def _run_basic(self, dataset, logger):
+    def _run_binsearch(self, dataset, logger):
         ''' The `run` method for 'fgsm'. '''
         # the attack is already configured in `config()`
         rs = []
@@ -85,11 +88,8 @@ class DistortionBenchmark(object):
             # use linear search to find an adversarial magnitude since fgsm do not play well with exponential search
             # The attack would be run with magnitude of:
             # [ init_distortion * 1, init_distortion * 2, ..., init_distortion * (2**search_steps) ].
-            # The 2**search_steps here intends to archive the same sematic as exponential search.
+            # The 2**search_steps here intends to archive the similar sematic as exponential search.
             for i in range(2**self.search_step):
-                if logger:
-                    begin = i_batch * len(xs)
-                    logger.info('search n={}..{}: i={}'.format(begin, begin + len(xs) - 1, i))
                 magnitude = self.init_distortion * (2**self.search_step - i)
                 # config the attack
                 self.attack.config(magnitude=magnitude)
@@ -112,14 +112,15 @@ class DistortionBenchmark(object):
                 xs_result[succ] = xs_adv[succ]
                 # update the smallest adversarial magnitude
                 hi[succ] = magnitude
+                if logger:
+                    begin = i_batch * len(xs)
+                    logger.info('linear search n={}..{}: i={}, succ={:.3f}'.format(
+                        begin, begin + len(xs) - 1, i, succ.astype(np.float).mean()))
 
             lo = hi - self.init_distortion
 
             # run binsearch to find the minimal adversarial magnitude
             for i in range(self.binsearch_step):
-                if logger:
-                    begin = i_batch * len(xs)
-                    logger.info('binsearch n={}..{}: i={}'.format(begin, begin + len(xs) - 1, i))
                 # config the attack
                 mi = (lo + hi) / 2
                 self.attack.config(magnitude=mi)
@@ -144,6 +145,103 @@ class DistortionBenchmark(object):
                 not_succ = np.logical_not(succ)
                 hi[succ] = mi[succ]
                 lo[not_succ] = mi[not_succ]
+                if logger:
+                    begin = i_batch * len(xs)
+                    logger.info('binary search n={}..{}: i={}, succ={:.3f}'.format(
+                        begin, begin + len(xs) - 1, i, succ.astype(np.float).mean()))
+
+            for x, x_result in zip(xs, xs_result):
+                if np.all(x_result == 0):  # all attacks failed
+                    rs.append(np.nan)
+                else:
+                    if self.distance_metric == 'l_inf':
+                        rs.append(np.max(np.abs(x_result - x)))
+                    else:
+                        rs.append(np.sqrt(np.sum((x_result - x)**2)))
+
+        return np.array(rs)
+
+    def _run_binsearch_alpha(self, dataset, logger):
+        ''' The `run` method for 'bim' and 'mim'. '''
+        # the attack is already configured in `config()`
+        iteration = self.attack.iteration
+
+        rs = []
+
+        iterator = dataset_to_iterator(dataset.batch(self.batch_size), self._session)
+        for i_batch, (_, xs, ys, ys_target) in enumerate(iterator):
+            # create numpy index for fetching the original and target label's logit value
+            ys_range = np.arange(0, self.batch_size * self.model.n_class, self.model.n_class)
+            ys_flatten = ys_range.astype(self.model.y_dtype.as_numpy_dtype) + ys
+            ys_target_flatten = ys_range.astype(self.model.y_dtype.as_numpy_dtype) + ys_target
+            del ys_range
+
+            lo = np.zeros(self.batch_size, dtype=np.float32)
+            hi = lo + self.init_distortion
+
+            # set xs_result to zeros initially, so that if the attack fails all the way down we could know it.
+            xs_result = np.zeros_like(xs)
+
+            # use exponential search to find an adversarial magnitude
+            for i in range(self.search_step):
+                # config the attack
+                self.attack.config(magnitude=hi, alpha=hi * 1.5 / iteration)
+                # run the attack
+                xs_adv = self.attack.batch_attack(xs, ys, ys_target)
+                logits = self._session.run(self._logits, feed_dict={self._xs_ph: xs_adv})
+                # check if attack succeed considering the confidence value
+                if self.goal == 'ut' or self.goal == 'tm':
+                    # for ut and tm goal, if the original label's logit is not the largest one, the example is
+                    # adversarial.
+                    succ = logits.max(axis=1) - logits.take(ys_flatten) > self.confidence
+                else:
+                    # for t goal, if the target label's logit is the largest one, the example is adversarial.
+                    logits_this = logits.take(ys_target_flatten)
+                    logits = logits.flatten()
+                    logits[ys_target_flatten] = np.nan
+                    logits_that = np.nanmax(logits.reshape((self.batch_size, -1)), axis=1)
+                    succ = logits_this - logits_that > self.confidence
+                # update the advsearial examples
+                xs_result[succ] = xs_adv[succ]
+                # if failed, use a larger magnitude for next iteration
+                not_succ = np.logical_not(succ)
+                lo[not_succ] = hi[not_succ]
+                hi[not_succ] *= 2
+                if logger:
+                    begin = i_batch * len(xs)
+                    logger.info('exponential search n={}..{}: i={}, succ={:.3f}'.format(
+                        begin, begin + len(xs) - 1, i, succ.astype(np.float).mean()))
+
+            # run binsearch to find the minimal adversarial magnitude
+            for i in range(self.binsearch_step):
+                # config the attack
+                mi = (lo + hi) / 2
+                self.attack.config(magnitude=mi, alpha=mi * 1.5 / iteration)
+                # run the attack
+                xs_adv = self.attack.batch_attack(xs, ys, ys_target)
+                logits = self._session.run(self._logits, feed_dict={self._xs_ph: xs_adv})
+                # check if attack succeed considering the confidence value
+                if self.goal == 'ut' or self.goal == 'tm':
+                    # for ut and tm goal, if the original label's logit is not the largest one, the example is
+                    # adversarial.
+                    succ = logits.max(axis=1) - logits.take(ys_flatten) > self.confidence
+                else:
+                    # for t goal, if the target label's logit is the largest one, the example is adversarial.
+                    logits_this = logits.take(ys_target_flatten)
+                    logits = logits.flatten()
+                    logits[ys_target_flatten] = np.nan
+                    logits_that = np.nanmax(logits.reshape((self.batch_size, -1)), axis=1)
+                    succ = logits_this - logits_that > self.confidence
+                # update the advsearial examples
+                xs_result[succ] = xs_adv[succ]
+                # update hi (if succeed) or lo (if not)
+                not_succ = np.logical_not(succ)
+                hi[succ] = mi[succ]
+                lo[not_succ] = mi[not_succ]
+                if logger:
+                    begin = i_batch * len(xs)
+                    logger.info('binary search n={}..{}: i={}, succ={:.3f}'.format(
+                        begin, begin + len(xs) - 1, i, succ.astype(np.float).mean()))
 
             for x, x_result in zip(xs, xs_result):
                 if np.all(x_result == 0):  # all attacks failed
