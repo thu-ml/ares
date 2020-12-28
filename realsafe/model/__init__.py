e1747b986ec5ad433394cfe459198723e0ff11b5
anonymous <xiao_yang@sjtu.edu.cn>
2020-12-28 16:03:10 +0800

    change

--- realsafe/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from realsafe.model.base import Classifier, ClassifierWithLogits
-from realsafe.model.loader import load_model_from_path
-
-__all__ = [
-    'Classifier', 'ClassifierWithLogits',
-    'load_model_from_path',
-]
